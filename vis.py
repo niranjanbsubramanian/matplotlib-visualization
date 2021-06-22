63b5ef41b47ec8359a03589e801e7ac61a322667
Niranjan B Subramanian <32201269+niranjanbsubramanian@users.noreply.github.com>
2021-06-22 21:06:36 +0530

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,1 +1,39 @@
+# Line Plot
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#number of ice creams sold in a week
+ice_cream = [35,33,65,44,75,88,101]
+
+plt.plot(ice_cream)
+plt.show()
+
+
+# Scatter Plot
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#load the dataset
+sal = pd.read_csv('/Salary_Data.csv')
+sal.head()
+
+#storing the values of experience and sal in a separate variable
+experience = sal['YearsExperience']
+salary = sal['Salary']
+
+plt.scatter(experience, salary)
+plt.show()
+
+# Bar Plot
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#number of ice creams sold in a week
+ice_cream = [35,33,65,44,75,88,101]
+days = ['Mon','Tues','Wed','Thur','Fri','Sat','Sun']
+
+plt.bar(days, ice_cream)
+plt.show()
